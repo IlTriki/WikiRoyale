bbe676e3e29761ed1e3a5edd290c2f20d22e2c5a
Noa Bou <noabou57@gmail.com>
2024-04-06 10:11:47 +0200

    debut frontend

--- FrontEnd/myApp/src/app/app.module.ts
+++ FrontEnd/myApp/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [
     provideClientHydration()
---
+++ FrontEnd/myApp/src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  // private property to store name value
+  private readonly _name: string;
+
+  /**
+   * Component constructor
+   */
+  constructor() {
+    this._name = 'Angular Fans';
+  }
+
+  /**
+   * Returns private property _name
+   */
+  get name(): string {
+    return this._name;
+  }
+
+  /**
+   * OnInit implementation
+   */
+  ngOnInit(): void {
+  }
+}
