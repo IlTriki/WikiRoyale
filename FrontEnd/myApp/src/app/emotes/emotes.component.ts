ab4ad41d4f2621c5e678d78ee282b8d8ed6c3b4d
Noa Bou <noabou57@gmail.com>
2024-04-10 12:14:56 +0200

    derniere modif emote

--- FrontEnd/myApp/src/app/emotes/emotes.component.ts
+++ FrontEnd/myApp/src/app/emotes/emotes.component.ts
@@ -9,39 +9,27 @@ import { Emote } from '../shared/types/emote.type';
   styleUrl: './emotes.component.css'
 })
 export class EmotesComponent {
-  // private property to store cards value
   private _emotes: Emote[];
-  // private property to store all backend URLs
   private readonly _backendURL: any;
 
-  /**
-   * Component constructor
-   */
   constructor(private _http: HttpClient) {
     this._emotes = [];
     this._backendURL = {};
 
-    // build backend base url
     let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
     if (environment.backend.port) {
       baseUrl += `:${environment.backend.port}`;
     }
 
-    // build all backend urls
     // @ts-ignore
     Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[ k ] = `${baseUrl}${environment.backend.endpoints[ k ]}`);
   }
 
-  /**
-   * Returns private property _cards
-   */
   get emotes(): Emote[] {
     return this._emotes;
   }
 
-  /**
-   * OnInit implementation
-   */
+
   ngOnInit(): void {
     this._http.get<Emote[]>(this._backendURL.allEmotes)
       .subscribe({ next: (emotes: Emote[]) => this._emotes = emotes });
